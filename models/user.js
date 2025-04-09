a8e35355c283b018f25bb95a8d163f647f129e13
Harshit Sah <harshitsah96@gmail.com>
2025-04-09 21:40:08 +0530

    First commit

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+
+    return db
+      .collection("users")
+      .insertOne(this);
+  }
+
+  static findUserById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) });
+      
+  }
+}
+
+module.exports = User;
